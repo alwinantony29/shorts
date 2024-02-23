fa84943ebcecd7472e9dfc9898148d22e1f3e202
Alwin Antony <110175978+alwinantony29@users.noreply.github.com>
2024-02-23 08:33:50 +0000

    added shad/cn and Slider component

--- src/components/shorts.js
+++ src/components/shorts.js
@@ -3,10 +3,11 @@ import { MdOutlineArrowRight } from "react-icons/md";
 import { IoIosShareAlt, IoMdPlay as PlayIcon, IoMdPause as PauseIcon, IoMdVolumeHigh, IoMdVolumeOff } from "react-icons/io";
 import { BiSolidLike, BiSolidDislike, BiSolidCommentDetail } from "react-icons/bi";
 import { BsThreeDotsVertical } from "react-icons/bs";
+import { Slider } from "../components/ui/slider";
 
 const Shorts = () => {
     return (
-        <div className='w-[25%] max-w-[315px] h-[80svh] rounded-md flex flex-col relative bg-slate-300 p-3'>
+        <div className='w-[25%] max-w-[315px] h-[80svh] rounded-md flex flex-col relative bg-slate-300 p-2'>
             <div className='flex justify-between '>
                 <PlayIcon />
                 <IoMdVolumeHigh />
@@ -30,13 +31,15 @@ const Shorts = () => {
                 </div>
             </div>
             <div className=' gap-5 items-center text-3xl flex-col-reverse flex absolute right-[-4rem] p-1 top-0 w-16 h-full'>
-                <button><BsThreeDotsVertical/></button>
-                <button><IoIosShareAlt/></button>
-                <button><BiSolidCommentDetail/></button>
-                <button><BiSolidDislike/></button>
+                <button><BsThreeDotsVertical /></button>
+                <button><IoIosShareAlt /></button>
+                <button><BiSolidCommentDetail /></button>
+                <button><BiSolidDislike /></button>
                 <button><BiSolidLike /></button>
             </div>
-
+            <div className='absolute left-0 bottom-0 px-[1%] w-full'>
+            <Slider  className=' w-[98%] mx-auto'/>
+            </div>
         </div>
     )
 }
