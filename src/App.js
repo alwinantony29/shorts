74c323e7328e201d66178786813511245efd261e
Alwin Antony <110175978+alwinantony29@users.noreply.github.com>
2024-02-23 11:44:52 +0000

    Added carousel for shorts

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import Shorts from "./components/shorts";
-
+import videos from "./data.json";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "./components/ui/carousel"
 function App() {
+  console.log({ videos })
   return (
-    <div className="flex p-5 min-h-screen w-screen items-center justify-center bg-slate-400">
-      <Shorts/>
+    <div
+      className="flex p-5 h-screen w-screen items-center justify-center bg-slate-700"
+    >
+      <Carousel
+        className='max-h-screen max-w-sm'
+        orientation='vertical'
+      >
+        <CarouselContent className='w-[350px] h-[90vh]'>
+          {Object.values(videos).map((video, index) => {
+            return <CarouselItem key={index} className="">
+              <Shorts {...video} />
+            </CarouselItem>
+          })}
+        </CarouselContent>
+        {/* <CarouselPrevious /> */}
+        {/* <CarouselNext /> */}
+      </Carousel>
     </div>
   );
 }
--- src/components/shorts.js
+++ src/components/shorts.js
@@ -5,40 +5,46 @@ import { BiSolidLike, BiSolidDislike, BiSolidCommentDetail } from "react-icons/b
 import { BsThreeDotsVertical } from "react-icons/bs";
 import { Slider } from "../components/ui/slider";
 
-const Shorts = () => {
+const Shorts = ({ src }) => {
+
     return (
-        <div className='w-[25%] max-w-[315px] h-[80svh] rounded-md flex flex-col relative bg-slate-300 p-2'>
-            <div className='flex justify-between z-10 '>
-                <PlayIcon />
-                <IoMdVolumeHigh />
-            </div>
-            <div className='absolute h-[25%] z-10 gap-2 flex-col bottom-0'>
-                <div className='flex gap-3 items-center  font-semibold'>
-                    <img className='h-9 w-9 rounded-full ' alt='profile pic' src='https://yt3.ggpht.com/yti/AGOGRCoiEx1X29gGYZQtGH3qoikIqIl5NqNa2hibMD8X2-4=s88-c-k-c0x00ffffff-no-rj' />
-                    <p className='text-sm'>@EpicPopcornReviews</p>
-                    <button className='rounded-2xl px-2 py-1 bg-white' >
-                        <span className='opacity-80 text-sm'>
-                            Subscribe
-                        </span>
-                    </button>
+        <div>
+
+            <div className='w-[80%] min-w-[300px] text-white max-w-[350px] h-[80svh] rounded-md flex flex-col relative bg-slate-300 p-2'>
+                <video className='inset-0 object-cover absolute w-full h-full top-0 left-0 rounded-md' src={src} autoPlay loop />
+
+                <div className='flex justify-between z-10 '>
+                    <PlayIcon />
+                    <IoMdVolumeHigh />
                 </div>
-                <div className='flex items-center gap-3 w-full font-semibold'>
-                    <MdOutlineArrowRight />
-                    <p>MCU 4/32 - He is SUCCESSFUL in</p>
+                <div className='absolute h-[25%] z-10 gap-2 flex-col bottom-0'>
+                    <div className='flex gap-3 items-center  font-semibold'>
+                        <img className='h-9 w-9 rounded-full ' alt='profile pic' src='https://yt3.ggpht.com/yti/AGOGRCoiEx1X29gGYZQtGH3qoikIqIl5NqNa2hibMD8X2-4=s88-c-k-c0x00ffffff-no-rj' />
+                        <p className='text-sm'>@EpicPopcornReviews</p>
+                        <button className='rounded-2xl px-2 py-0.5 bg-white' >
+                            <span className='opacity-80 text-sm text-black'>
+                                Subscribe
+                            </span>
+                        </button>
+                    </div>
+                    <div className='flex items-center gap-3 w-full font-semibold'>
+                        <MdOutlineArrowRight />
+                        <p>MCU 4/32 - He is SUCCESSFUL in</p>
+                    </div>
+                    <div className='flex w-full font-semibold text-wrap '>
+                        <p>World PEACE #ironman2 #movie #featuremovie #film #mcu</p>
+                    </div>
                 </div>
-                <div className='flex w-full font-semibold text-wrap '>
-                    <p>World PEACE #ironman2 #movie #featuremovie #film #mcu</p>
+                <div className='action-bar gap-5 items-center text-3xl flex-col-reverse flex absolute right-[-4rem] p-1 top-0 w-16 h-full'>
+                    <button><BsThreeDotsVertical /></button>
+                    <button><IoIosShareAlt /></button>
+                    <button><BiSolidCommentDetail /></button>
+                    <button><BiSolidDislike /></button>
+                    <button><BiSolidLike /></button>
                 </div>
-            </div>
-            <div className=' gap-5 items-center text-3xl flex-col-reverse flex absolute right-[-4rem] p-1 top-0 w-16 h-full'>
-                <button><BsThreeDotsVertical /></button>
-                <button><IoIosShareAlt /></button>
-                <button><BiSolidCommentDetail /></button>
-                <button><BiSolidDislike /></button>
-                <button><BiSolidLike /></button>
-            </div>
-            <div className='absolute left-0 bottom-0 px-[1%] w-full z-10'>
-            <Slider  className=' w-[98%] mx-auto'/>
+                <div className='absolute left-0 bottom-0 px-[1%] w-full z-10'>
+                    <Slider className=' w-[98%] mx-auto' />
+                </div>
             </div>
         </div>
     )
