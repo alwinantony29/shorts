0f569d22bbf8ad2898f14fcce016823a998bbbeb
Alwin Antony <110175978+alwinantony29@users.noreply.github.com>
2024-02-23 06:58:13 +0000

    added action bar

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Shorts from "./components/shorts";
 
 function App() {
   return (
-    <div className="flex p-5 min-h-screen w-screen items-center justify-center">
+    <div className="flex p-5 min-h-screen w-screen items-center justify-center bg-slate-400">
       <Shorts/>
     </div>
   );
--- src/components/shorts.js
+++ src/components/shorts.js
@@ -1,24 +1,40 @@
 import React from 'react'
 import { MdOutlineArrowRight } from "react-icons/md";
+import { IoIosShareAlt, IoMdPlay as PlayIcon, IoMdPause as PauseIcon, IoMdVolumeHigh, IoMdVolumeOff } from "react-icons/io";
+import { BiSolidLike, BiSolidDislike, BiSolidCommentDetail } from "react-icons/bi";
+import { BsThreeDotsVertical } from "react-icons/bs";
 
 const Shorts = () => {
     return (
-        <div className='w-[30%] h-[80svh] rounded-md flex-col relative'>
-            shorts
-            <div className='absolute w-full h-[25%] p-5 gap-2 flex-col bottom-0'>
-                <div className='flex gap-3 items-center w-full font-semibold'>
-                    <img className='h-9 w-9 rounded-full' alt='profile pic' src='https://yt3.ggpht.com/yti/AGOGRCoiEx1X29gGYZQtGH3qoikIqIl5NqNa2hibMD8X2-4=s88-c-k-c0x00ffffff-no-rj' />
-                    <p>@EpicPopcornReviews</p>
-                    <button className='rounded-2xl px-2 py-1 ' >Subscribe</button>
+        <div className='w-[25%] max-w-[315px] h-[80svh] rounded-md flex flex-col relative bg-slate-300 p-3'>
+            <div className='flex justify-between '>
+                <PlayIcon />
+                <IoMdVolumeHigh />
+            </div>
+            <div className='absolute h-[25%]  gap-2 flex-col bottom-0'>
+                <div className='flex gap-3 items-center  font-semibold'>
+                    <img className='h-9 w-9 rounded-full ' alt='profile pic' src='https://yt3.ggpht.com/yti/AGOGRCoiEx1X29gGYZQtGH3qoikIqIl5NqNa2hibMD8X2-4=s88-c-k-c0x00ffffff-no-rj' />
+                    <p className='text-sm'>@EpicPopcornReviews</p>
+                    <button className='rounded-2xl px-2 py-1 bg-white' >
+                        <span className='opacity-70'>
+                            Subscribe
+                        </span>
+                    </button>
                 </div>
                 <div className='flex items-center gap-3 w-full font-semibold'>
                     <MdOutlineArrowRight />
                     <p>MCU 4/32 - He is SUCCESSFUL in</p>
                 </div>
-                <div className='flex w-full font-semibold'>
+                <div className='flex w-full font-semibold text-wrap '>
                     <p>World PEACE #ironman2 #movie #featuremovie #film #mcu</p>
                 </div>
-
+            </div>
+            <div className=' gap-5 items-center text-3xl flex-col-reverse flex absolute right-[-4rem] p-1 top-0 w-16 h-full'>
+                <button><BsThreeDotsVertical/></button>
+                <button><IoIosShareAlt/></button>
+                <button><BiSolidCommentDetail/></button>
+                <button><BiSolidDislike/></button>
+                <button><BiSolidLike /></button>
             </div>
 
         </div>
